86f0bbe3ea08ecf7dff1c05d8b8046f7b4d844ec
maxwen <max.weninger@gmail.com>
2018-01-30 02:48:30 +0000

    Radioactive: custom header image provider
    reads image uri and saves it in the file custom_file_header_image
    in /data/user_de/0/com.android.systemui/files/file_header_image
    must be copied else loading on reboot will fail cause storage
    may not be mounted
    
    Change-Id: I1304f686102924275fa6ecdbc369e55df82987c3
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- src/org/cesium/config/center/fragments/UITunerFragment.java
+++ src/org/cesium/config/center/fragments/UITunerFragment.java
@@ -22,6 +22,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.provider.Settings;
+import android.net.Uri;
 import android.text.TextUtils;
 import androidx.preference.*;
 
@@ -56,6 +57,9 @@ public class UITunerFragment extends SettingsPreferenceFragment
     private static final String CUSTOM_HEADER_PROVIDER = "custom_header_provider";
     private static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
     private static final String CUSTOM_HEADER_ENABLED = "status_bar_custom_header";
+    private static final String FILE_HEADER_SELECT = "file_header_select";
+    private static final int REQUEST_PICK_IMAGE = 0;
+
     public static final String TAG = "UITunerFragment";
 
     private ContentResolver mResolver;
@@ -63,6 +67,8 @@ public class UITunerFragment extends SettingsPreferenceFragment
     private ScrollAppsViewPreference mAspectRatioApps;
     private Preference mHeaderBrowse;
     private ListPreference mDaylightHeaderPack;
+    private Preference mFileHeader;
+    private String mFileHeaderProvider;
     private CustomSeekBarPreference mHeaderShadow;
     private ListPreference mHeaderProvider;
     private String mDaylightHeaderProvider;
@@ -75,7 +81,6 @@ public class UITunerFragment extends SettingsPreferenceFragment
         PreferenceScreen prefScreen = getPreferenceScreen();
 
         mHeaderBrowse = findPreference(CUSTOM_HEADER_BROWSE);
-        mHeaderBrowse.setEnabled(isBrowseHeaderAvailable());
 
         mHeaderEnabled = (SwitchPreference) findPreference(CUSTOM_HEADER_ENABLED);
         mHeaderEnabled.setOnPreferenceChangeListener(this);
@@ -99,17 +104,21 @@ public class UITunerFragment extends SettingsPreferenceFragment
         mHeaderShadow.setOnPreferenceChangeListener(this);
 
         mDaylightHeaderProvider = getResources().getString(R.string.daylight_header_provider);
+        mFileHeaderProvider = getResources().getString(R.string.file_header_provider);
         String providerName = Settings.System.getString(resolver,
                 Settings.System.STATUS_BAR_CUSTOM_HEADER_PROVIDER);
         if (providerName == null) {
             providerName = mDaylightHeaderProvider;
         }
+        mHeaderBrowse.setEnabled(isBrowseHeaderAvailable() && !providerName.equals(mFileHeaderProvider));
         mHeaderProvider = (ListPreference) findPreference(CUSTOM_HEADER_PROVIDER);
         int valueIndex = mHeaderProvider.findIndexOfValue(providerName);
         mHeaderProvider.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
         mHeaderProvider.setSummary(mHeaderProvider.getEntry());
         mHeaderProvider.setOnPreferenceChangeListener(this);
         mDaylightHeaderPack.setEnabled(providerName.equals(mDaylightHeaderProvider));
+        mFileHeader = findPreference(FILE_HEADER_SELECT);
+        mFileHeader.setEnabled(providerName.equals(mFileHeaderProvider));
     }
 
     private void updateHeaderProviderSummary(boolean headerEnabled) {
@@ -182,8 +191,10 @@ public class UITunerFragment extends SettingsPreferenceFragment
                 int valueIndex = mHeaderProvider.findIndexOfValue(value);
                 mHeaderProvider.setSummary(mHeaderProvider.getEntries()[valueIndex]);
                 mDaylightHeaderPack.setEnabled(value.equals(mDaylightHeaderProvider));
+                mHeaderBrowse.setEnabled(!value.equals(mFileHeaderProvider));
                 mHeaderBrowse.setTitle(valueIndex == 0 ? R.string.custom_header_browse_title : R.string.custom_header_pick_title);
                 mHeaderBrowse.setSummary(valueIndex == 0 ? R.string.custom_header_browse_summary_new : R.string.custom_header_pick_summary);
+                mFileHeader.setEnabled(value.equals(mFileHeaderProvider));
                 return true;
 
             case CUSTOM_HEADER_ENABLED:
@@ -194,6 +205,17 @@ public class UITunerFragment extends SettingsPreferenceFragment
           default:
         	return false;
         }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mFileHeader) {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, REQUEST_PICK_IMAGE);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 
     private boolean isBrowseHeaderAvailable() {
@@ -256,5 +278,16 @@ public class UITunerFragment extends SettingsPreferenceFragment
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == REQUEST_PICK_IMAGE) {
+            if (resultCode != Activity.RESULT_OK) {
+                return;
+            }
+            final Uri imageUri = result.getData();
+            Settings.System.putString(getContentResolver(), Settings.System.STATUS_BAR_FILE_HEADER_IMAGE, imageUri.toString());
+        }
     }
 }
